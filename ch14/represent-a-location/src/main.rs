461041fe9c5ea11836e319345449fc7c8d8e3156
Asad Hayat <asadhayat.1068@gmail.com>
2023-09-26 03:09:15 +0500

    Ch14 Excercise - Bonus challange - Implement Display trait for Location Enum

--- ch14/represent-a-location/src/main.rs
+++ ch14/represent-a-location/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 enum Location {
     Unknown,
     Anonymous,
@@ -14,13 +15,26 @@ impl Location {
     }
 }
 
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::Unknown => write!(f, "Location is Unknown"),
+            Self::Anonymous => write!(f, "Location is anonymous"),
+            Self::Known(lat, lon) => write!(f, "Location is ({lat},{lon})"),
+        }
+    }
+}
+
 fn main() {
     let address = Location::Unknown;
     address.display();
+    println!("Display: {}", address);
 
     let address = Location::Anonymous;
     address.display();
+    println!("Display: {}", address);
 
     let address = Location::Known(28.608295, -80.604177);
     address.display();
+    println!("Display: {}", address);
 }
